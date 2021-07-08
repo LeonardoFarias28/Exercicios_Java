87ded4b94a01b1ce1b542661f86e72c611554c6f
Leonardo-Farias <leonardosfariaas@gmail.com>
2021-07-07 23:54:09 -0300

    Mostrar_Numeros_Impares
    Exercício utilizando for e if para mostrar os números ímpares.

---
+++ src/Estrutura_For/Mostrar_Numeros_Impares.java
@@ -0,0 +1,23 @@
+package Estrutura_For;
+
+import java.util.Scanner;
+
+public class Mostrar_Numeros_Impares {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		int x = sc.nextInt();
+		for (int i = 0; i < x; i++) {
+
+			if (i % 2 == 0) {
+				i += 1;
+				System.out.println(i);
+			}
+			
+		}
+		sc.close();
+	}
+
+}
