f51c3cafad492a7d38b4efbd648474b149a7e742
Leonardo-Farias <leonardosfariaas@gmail.com>
2021-07-07 23:44:03 -0300

    Estrutura_For
    Exercício contagem de 0 a 4 utilizando for.

---
+++ src/Estrutura_For/UtilizandoForContagem.java
@@ -0,0 +1,18 @@
+package Estrutura_For;
+
+import java.util.Scanner;
+
+public class UtilizandoForContagem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		for(int i = 0; i<5; i++) {
+			System.out.println("O valor de i: " + i);
+		}
+		sc.close();
+	}
+
+}
